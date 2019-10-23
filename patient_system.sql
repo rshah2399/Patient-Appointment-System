579a386ec0b434acdd18a520164634d64dcb0bd5
rshah2399 <rshah2399@gmail.com>
2019-10-23 06:51:12 +0530

    updated sql

--- patient_system.sql
+++ patient_system.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 21, 2019 at 09:40 AM
+-- Generation Time: Oct 23, 2019 at 03:19 AM
 -- Server version: 10.4.6-MariaDB
 -- PHP Version: 7.3.9
 
@@ -35,40 +35,24 @@ CREATE TABLE `appointment` (
   `gender` varchar(10) NOT NULL,
   `email` varchar(20) NOT NULL,
   `date` varchar(20) NOT NULL,
-  `time` varchar(20) NOT NULL
+  `time` varchar(20) NOT NULL,
+  `report` varchar(300) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `appointment`
 --
 
-INSERT INTO `appointment` (`a_id`, `name`, `phone`, `gender`, `email`, `date`, `time`) VALUES
-(1, 'Rahil Shah', 0, 'male', 'rshah2399@gmail.com', '2019-09-29', '9:00'),
-(2, 'Krinal Shah', 0, 'male', 'krinalshah97@gmail.c', '2019-09-28', '9:00'),
-(3, 'Rahil Shah', 0, 'male', 'rshah2399@gmail.com', '2019-09-28', '14:30'),
-(4, 'Priti shah', 0, 'male', 'wes@asd.com', '2019-09-28', '9:00'),
-(5, 'Priti shah', 0, 'male', 'wes@asd.com', '2019-09-28', '9:00');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
-
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
-  `name` char(50) NOT NULL,
-  `phone` char(10) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`id`, `name`, `phone`, `email`, `password`) VALUES
-(1, 'Rahil Shah', '9757235318', 'rshah2399@gmail.com', 'qwerty123');
+INSERT INTO `appointment` (`a_id`, `name`, `phone`, `gender`, `email`, `date`, `time`, `report`) VALUES
+(1, 'Rahil Shah', 9870753456, 'male', 'rshah2399@gmail.com', '2019-09-29', '11:00', 'diagonised'),
+(2, 'Krinal Shah', 9865432145, 'female', 'krinalshah97@gmail.c', '2019-09-28', '9:30', 'fit'),
+(3, 'Rahil Shah', 7658678987, 'male', 'rshah2399@gmail.com', '2019-09-28', '14:30', 'fully fit'),
+(4, 'Priti shah', 7685456784, 'female', 'wes@asd.com', '2019-09-28', '10:00', 'malaria'),
+(5, 'Priti shah', 8098977687, 'male', 'wes@asd.com', '2019-09-28', '9:00', 'recovered'),
+(6, 'Rahil Shah', 8765439234, 'male', 'rshah2399@gmail.com', '2019-09-28', '13:30', 'dead'),
+(7, 'Rahil Shah', 9870767865, 'male', 'rshah2399@gmail.com', '2019-09-28', '14:00', 'dengue'),
+(8, 'Krinal Shah', 9876543213, 'female', 'krinalshah97@gmail.c', '2019-10-03', '9:00', 'loose motions'),
+(9, 'Krinal Shah', 6509789876, 'female', 'krinalshah97@gmail.c', '2019-10-03', '9:30', 'high fever');
 
 --
 -- Indexes for dumped tables
@@ -81,14 +65,6 @@ ALTER TABLE `appointment`
   ADD PRIMARY KEY (`a_id`);
 
 --
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `phone` (`phone`),
-  ADD UNIQUE KEY `email` (`email`);
-
---
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -96,13 +72,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `appointment`
 --
 ALTER TABLE `appointment`
-  MODIFY `a_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `a_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
